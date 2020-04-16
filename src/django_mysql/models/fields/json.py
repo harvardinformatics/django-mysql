be17fafa555474f05a2f297d269aeaeef9bf46c1
Aaron Kitzmiller <aaron.kitzmiller@gmail.com>
2020-04-16 18:10:58 -0400

    jeebus

--- src/django_mysql/models/fields/json.py
+++ src/django_mysql/models/fields/json.py
@@ -92,7 +92,7 @@ class JSONField(Field):
     def _check_json_encoder_decoder(self):
         errors = []
 
-        if hasattr('allow_nan', self.json_encoder) and self.json_encoder.allow_nan:
+        if hasattr(self.json_encoder, 'allow_nan') and self.json_encoder.allow_nan:
             errors.append(
                 checks.Error(
                     "Custom JSON encoder should have allow_nan=False as MySQL "
